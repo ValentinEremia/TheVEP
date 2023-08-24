8ecfddac1d3d430d9273dc141fac9e72ae169e8e
ValentinEremia <valentineremia@yahoo.com>
2023-08-24 21:06:04 +0300

    #

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -45,7 +45,7 @@ const BlogPostCard = ({ link, image, title, summary }: Props) => {
         <div className="p-6">
           <h4 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-black dark:text-white">
             {title}
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               className="ml-1 h-4 w-4 inline-block fill-current"
@@ -57,7 +57,7 @@ const BlogPostCard = ({ link, image, title, summary }: Props) => {
                   <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
                 </g>
               </g>
-            </svg>
+            </svg> */}
           </h4>
           <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{summary}</p>
           
