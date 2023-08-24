a41c6b5467659da18b9c29b2d88e5e24c3625e32
ValentinEremia <valentineremia@yahoo.com>
2023-08-24 21:08:43 +0300

    #

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -26,7 +26,7 @@ const ProjectCard = ({ link, image, title, description }: Props) => {
       className="p-4 mx-auto  sm:w-1/2 max-w-[34rem]"
       showIcon={false}
     >
-      <div className="h-full overflow-hidden border-[1px] border-gray-200 rounded-md dark:border-gray-800 hover:border-primary-500 dark:hover:border-primary-400">
+      <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md dark:border-gray-800 hover:border-primary-500 dark:hover:border-primary-400">
         <Image
           alt={title}
           src={image}
