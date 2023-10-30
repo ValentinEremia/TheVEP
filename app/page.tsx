ec717cf245fa13c9b07806b809c10f7f560f0975
ValentinEremia <valentineremia@yahoo.com>
2023-10-30 20:24:34 +0200

    #

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function HomePage() {
         <Hero />
       </section>
       <LatestPostHeroSection />
-      <Particles
+      {/* <Particles
 				className="absolute inset-0 -z-10 h-full   animate-fade-in text-white dark:!text-fuchsia-600  "
 				quantity={70}
-			/>
+			/> */}
     </>
   );
 }
