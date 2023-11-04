c756602784c3fa7f5a6f59cd7ca9460ffafce7ff
ValentinEremia <valentineremia@yahoo.com>
2023-11-04 13:16:41 +0200

    CV & Back Button on Blog's

--- components/DownloadCvBtn.tsx
+++ components/DownloadCvBtn.tsx
@@ -10,7 +10,7 @@ className="text-violet-700 dark:text-violet-500"
 href={"CV-Valentin EREMIA.pdf"} locale={false} download='CV-Valentin EREMIA.pdf'
 aria-label={`Download my CV`}  title={`Download my CV`}>
  
-Download my CV
+Download my CV {' '}
 </CustomLink>
   );
 }
