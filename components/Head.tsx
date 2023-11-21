b66aae263aad6755648906cade80f147e863ab9c
ValentinEremia <valentineremia@yahoo.com>
2023-11-21 21:08:09 +0200

    #19171b

--- components/Head.tsx
+++ components/Head.tsx
@@ -9,11 +9,12 @@ export class Head extends Component {
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
         <meta name="description" content="description of your project" />
-        <meta name="theme-color" content="#000" />
+        <meta name="theme-color" content="#19171b" />
         <title>Vali's Blog & Portfolio</title>
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+         
       </head>
     );
   }
