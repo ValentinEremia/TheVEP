9f5072ff1cf63f56fd7b0f0d4931a9124a211d91
ValentinEremia <valentineremia@yahoo.com>
2023-11-27 19:43:03 +0200

    #

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,25 +1,28 @@
-import Container from '@/components/Container'
-import PageTitle from '@/components/PageTitle'
-import React from 'react'
+import Container from "@/components/Container";
+import PageTitle from "@/components/PageTitle";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function ServicesPage({}: Props) {
   return (
     <Container>
+      <div className="h-[88svh] ">
 
-
-
-      <div className="h-screen flex items-center justify-center">
-    <h1 className="text-3xl text-center font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       
-      Services Page <br /> Under Construction <br />
-      <div className="h-4" />
-        <span className="text-5xl" role="img" aria-label="roadwork sign">
-          🚧
-        </span>
-      </h1>
-    </div>
+      <div className=" z-10 pt-6 pb-8 space-y-2 md:space-y-5">
+        <PageTitle>Services</PageTitle>
+      </div>
+
+      <div className=" h-full flex items-center justify-center">
+        <h1 className="text-3xl text-center font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            Under Construction <br />
+          <div className="h-4" />
+          <span className="text-5xl" role="img" aria-label="roadwork sign">
+            🚧
+          </span>
+        </h1>
+      </div></div>
     </Container>
-  )
+  );
 }
--- components/Head.tsx
+++ components/Head.tsx
@@ -8,7 +8,7 @@ export class Head extends Component {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="description of your project" />
+        <meta name="description" content="Hey! I'm Valentin 👋! A mobile and web developer with a strong focus in front-end and system design." />
         <meta name="theme-color" content="#000" />
         <title>Vali's Blog & Portfolio</title>
         <link rel="manifest" href="/manifest.json" />
